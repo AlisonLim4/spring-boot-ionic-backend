1f439ec711ffb271a813260030e38293ca9ff181
AlisonLima <alison.juniolima@gmail.com>
2021-11-11 22:53:36 -0300

    REST funcionando e primeira classe de dominio

---
+++ src/main/java/com/alisonlima/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,29 @@
+package com.alisonlima.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alisonlima.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping
+public class CategoriaResource {
+
+	@RequestMapping(value="/categorias",method = RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
+
+}
