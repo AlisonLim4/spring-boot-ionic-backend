9d2fb1dbe218500b88d86c51efe23b18b90f9760
AlisonLima <alison.juniolima@gmail.com>
2022-01-04 11:10:07 -0300

    Operação de instanciação criada

--- src/main/java/com/alisonlima/cursomc/CursomcApplication.java
+++ src/main/java/com/alisonlima/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.alisonlima.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alisonlima.cursomc.domain.Categoria;
+import com.alisonlima.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		// salvando as categorias no Banco
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
