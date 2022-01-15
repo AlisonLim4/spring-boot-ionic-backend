b87dfd9581912a6747f41d0a1e2065a81cd1d74a
AlisonLima <alison.juniolima@gmail.com>
2022-01-15 18:20:45 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/alisonlima/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/alisonlima/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,15 @@
 package com.alisonlima.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alisonlima.cursomc.domain.Categoria;
 import com.alisonlima.cursomc.services.CategoriaService;
@@ -25,5 +26,14 @@ public class CategoriaResource {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
-
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	
 }
