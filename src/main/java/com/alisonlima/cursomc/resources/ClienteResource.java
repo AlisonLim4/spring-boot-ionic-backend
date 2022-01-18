5228014675683838ed0eaca32cb31c488f46a4b5
AlisonLima <alison.juniolima@gmail.com>
2022-01-18 17:32:54 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/alisonlima/cursomc/resources/ClienteResource.java
+++ src/main/java/com/alisonlima/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.alisonlima.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alisonlima.cursomc.domain.Cliente;
 import com.alisonlima.cursomc.dto.ClienteDTO;
+import com.alisonlima.cursomc.dto.ClienteNewDTO;
 import com.alisonlima.cursomc.services.ClienteService;
 
 @RestController
@@ -30,6 +33,14 @@ public class ClienteResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
--- src/main/java/com/alisonlima/cursomc/services/ClienteService.java
+++ src/main/java/com/alisonlima/cursomc/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.alisonlima.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.alisonlima.cursomc.domain.Cidade;
 import com.alisonlima.cursomc.domain.Cliente;
+import com.alisonlima.cursomc.domain.Endereco;
+import com.alisonlima.cursomc.domain.enums.TipoCliente;
 import com.alisonlima.cursomc.dto.ClienteDTO;
+import com.alisonlima.cursomc.dto.ClienteNewDTO;
 import com.alisonlima.cursomc.repositories.ClienteRepository;
+import com.alisonlima.cursomc.repositories.EnderecoRepository;
 import com.alisonlima.cursomc.services.exceptions.DataIntegrityException;
 
 @Service
@@ -20,6 +27,9 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	// metodo de busca
 	public Cliente find(Integer id) {
@@ -28,7 +38,16 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new com.alisonlima.cursomc.services.exceptions.ObjectNotFoundException(
 				"Objeto não Encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
+	
+	// metodo de inserção
+		@Transactional
+		public Cliente insert(Cliente obj) {
+			obj.setId(null);
+			obj = repo.save(obj);
+			enderecoRepository.saveAll(obj.getEnderecos());
+			return obj;
 
+		}
 	// metodo de atualização
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
@@ -57,12 +76,27 @@ public class ClienteService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerpage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
-
+	
+	//metodos auxiliares
 	public Cliente fromDTO(ClienteDTO objDTO) {
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(),null, null);
 	}
 	
-	
+	//sobrecarga de metodo
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = new Cidade(objDTO.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if (objDTO.getTelefone2() != null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if (objDTO.getTelefone3() != null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		return cli;
+	}
 	//metodos auxiliares
 	
 	private void updateData(Cliente newObj,Cliente obj) {
