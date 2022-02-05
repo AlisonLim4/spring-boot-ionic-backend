6cd12747f1cf637f606c65027f281d7060c052a9
AlisonLima <alison.juniolima@gmail.com>
2022-02-05 18:10:10 -0300

    Validação customizada: email nao repetido na Atualização de Cliente

--- src/main/java/com/alisonlima/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/alisonlima/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.alisonlima.cursomc.domain.Cliente;
+import com.alisonlima.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
