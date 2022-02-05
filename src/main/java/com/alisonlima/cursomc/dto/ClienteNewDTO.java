b04ace69c164a878bf6969d01b4661d1991dd99c
AlisonLima <alison.juniolima@gmail.com>
2022-02-05 14:48:26 -0300

    Validação customizada de cpfOuCnpj na inserção de Cliente

--- src/main/java/com/alisonlima/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/alisonlima/cursomc/dto/ClienteNewDTO.java
@@ -2,23 +2,46 @@ package com.alisonlima.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.alisonlima.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message = "Preenchimento Obrigatório")
+	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 Caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Preenchimento Obrigatório")
+	@Email(message = "Email inválido.")
 	private String email;
+	
+	@NotEmpty(message = "Preenchimento Obrigatório")
 	private String cpfOuCnpj;
+
 	private Integer tipo;
 	
+	@NotEmpty(message = "Preenchimento Obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message = "Preenchimento Obrigatório")
 	private String numero;
+	
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message = "Preenchimento Obrigatório")
 	private String cep;
 	
-	
+	@NotEmpty(message = "Preenchimento Obrigatório")
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
 	
---
+++ src/main/java/com/alisonlima/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,40 @@
+package com.alisonlima.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.alisonlima.cursomc.domain.enums.TipoCliente;
+import com.alisonlima.cursomc.dto.ClienteNewDTO;
+import com.alisonlima.cursomc.resources.exception.FieldMessage;
+import com.alisonlima.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		// inclua os testes aqui, inserindo erros na lista
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
